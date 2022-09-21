f500601d5ad3ee4ff268c607ea28e2de372fcc0a
Welber33 <jwwork21@gmail.com>
2022-09-21 16:37:57 -0300

    Building the Hero Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>João Welber Web Portfolio</title>
       </Head>
 
     
-    <Header />
+      <Header />
 
-    {/** Hero */}
+      {/** Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
-    {/** About */}
+      {/** About */}
 
-    {/** Experience */}
+      {/** Experience */}
 
-    {/** Skills */}
+      {/** Skills */}
 
-    {/** Projects */}
+      {/** Projects */}
 
-    {/** Contact Me */}
+      {/** Contact Me */}
     </div>
   )
 }
