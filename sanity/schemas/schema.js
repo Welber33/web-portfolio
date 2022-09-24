bd6894a3ecfa076e9a7cfe3e038981e8753104b2
Welber33 <jwwork21@gmail.com>
2022-09-24 13:30:17 -0300

    Implementing Sanity - Changn' and Addn' in Schemas

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,21 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import pageInfo from './pageInfo'
+import skill from './skill'
+import experience from './experience'
+import social from './social'
+import project from './project' 
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    pageInfo,
+    skill,
+    experience,
+    social,
+    project,
+    
+  ]),
+})
